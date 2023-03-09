5b1baa9225c9f3028dfd7b8a9959ab2813f54144
Fernando Silva <fernnlcs@gmail.com>
2023-03-09 00:07:55 -0300

    Deleted unnecessary files

--- bin/analysis_of_algorithms_exercises.dart
+++
@@ -1,5 +0,0 @@
-import 'package:analysis_of_algorithms_exercises/analysis_of_algorithms_exercises.dart' as analysis_of_algorithms_exercises;
-
-void main(List<String> arguments) {
-  print('Hello world: ${analysis_of_algorithms_exercises.calculate()}!');
-}
--- test/analysis_of_algorithms_exercises_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:analysis_of_algorithms_exercises/analysis_of_algorithms_exercises.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
